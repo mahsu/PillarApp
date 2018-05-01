ad655098aee46d75ea3f39e95fef4f0c9fd4cde8
Matthew Hsu <gears88@gmail.com>
2018-04-30 21:52:16 -0400

    Add bluetooth lib

--- client/android/app/src/main/java/com/pillar/MainApplication.java
+++ client/android/app/src/main/java/com/pillar/MainApplication.java
@@ -3,6 +3,7 @@ package com.pillar;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.polidea.reactnativeble.BlePackage;
 import org.reactnative.camera.RNCameraPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BlePackage(),
             new RNCameraPackage()
       );
     }
