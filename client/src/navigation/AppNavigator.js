bf19a8909b0653699ba06b52956135ae68559f9f
Matthew Hsu <gears88@gmail.com>
2018-05-03 21:23:31 -0400

    Push notifications not working

--- client/src/navigation/AppNavigator.js
+++ client/src/navigation/AppNavigator.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 
 import { StackNavigator } from "react-navigation";
 import FooterTabNavigator from "./FooterTabNavigator";
+import InternalDev from "../screens/InternalDev";
 
 const AppNavigator = StackNavigator(
     {
+        InternalDev: {screen: InternalDev},
         FooterTabs: {screen: FooterTabNavigator},
     },
     {
-        initialRouteName: "FooterTabs",
+        initialRouteName: "InternalDev",
         headerMode: "none",
 
     }
